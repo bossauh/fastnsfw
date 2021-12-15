9c7d7f196caa50381a8b31f40f7a7930b2de7e45
matt <philmattdev@gmail.com>
2021-12-16 02:56:27 +0800

    version update

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
     name="fastnsfw",
     packages=["fastnsfw"],
-    version="0.4",
+    version="0.5",
     license="MIT",
     description="NSFW Detection Library",
     author="Philippe Mathew",
     author_email="philmattdev@gmail.com",
     url="https://github.com/bossauh/fastnsfw",
-    download_url="https://github.com/bossauh/fastnsfw/archive/refs/tags/v_04.tar.gz",
+    download_url="https://github.com/bossauh/fastnsfw/archive/refs/tags/v_05.tar.gz",
     keywords=["nsfw", "detection"],
     install_requires=[
         "fluxhelper",
