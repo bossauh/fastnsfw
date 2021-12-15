a3d23708a1cb6b1de2548cd27e136c98a82540d1
matt <philmattdev@gmail.com>
2021-12-15 23:53:43 +0800

    remove nsfw_model from setup

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # FastNSFW
-A nsfw detection library that builds on top of [nsfw_model](https://github.com/GantMan/nsfw_model). It does this by providing nsfw detection not just for images but also for videos.
+A nsfw detection library that builds on top of [nsfw_model](https://github.com/GantMan/nsfw_model). It does this by providing nsfw detection not just for images but also for videos. You must install the mentioned nsfw_model first in order to use this.
 
 This project is made for a discord bot and is currently undocumented.
--- setup.py
+++ setup.py
@@ -2,16 +2,15 @@ from distutils.core import setup
 setup(
     name="fastnsfw",
     packages=["fastnsfw"],
-    version="0.1",
+    version="0.2",
     license="MIT",
     description="NSFW Detection Library",
     author="Philippe Mathew",
     author_email="philmattdev@gmail.com",
     url="https://github.com/bossauh/fastnsfw",
-    download_url="https://github.com/bossauh/fastnsfw/archive/refs/tags/v_01.tar.gz",
+    download_url="https://github.com/bossauh/fastnsfw/archive/refs/tags/v_02.tar.gz",
     keywords=["nsfw", "detection"],
     install_requires=[
-        "nsfw-model @ git+https://github.com/GantMan/nsfw_model.git",
         "fluxhelper",
         "pillow",
         "imagehash",
